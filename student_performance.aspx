611f366eed2aecabf7a01c6f61f0b2a5e3b85718
gowtham1582000 <gowtham1582000@gmail.com>
2023-08-13 01:24:24 +0530

    dsa

---
+++ student_performance.aspx
@@ -0,0 +1,158 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="student_performance.aspx.vb" Inherits="MyWebAppDemo1.student_performance" %>
+
+<%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+        }
+
+        .container {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+
+        #form1 {
+            background-color: #fff;
+            border-radius: 10px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+            padding: 20px;
+        }
+
+        .form-container {
+            width: 40%;
+            padding: 20px;
+            align-items: center;
+        }
+
+        .chart-container {
+            width: 55%;
+        }
+
+        label {
+            font-weight: bold;
+            display: block;
+            margin-bottom: 5px;
+        }
+
+        input[type="text"] {
+            width: 50%;
+            padding: 10px;
+            margin-bottom: 15px;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+        }
+
+        .button {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+
+            .button:hover {
+                background-color: #45a049;
+            }
+
+        .gridview {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+            .gridview th, .gridview td {
+                padding: 8px;
+                border: 1px solid #ccc;
+                text-align: left;
+            }
+
+            .gridview th {
+                background-color: #f2f2f2;
+                font-weight: bold;
+            }
+
+        .form-chart-container {
+            display: flex;
+            align-items: flex-start;
+            justify-content: space-between;
+        }
+
+        .form-container {
+            flex: 1;
+            padding: 20px;
+            box-sizing: border-box;
+        }
+
+        .chart-container {
+            flex: 1;
+            padding: 20px;
+            box-sizing: border-box;
+        }
+         h1 {
+            font-size: 24px;
+            text-align: center;
+            margin-bottom: 20px;
+            color: #333; /* Change the color as desired */
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Student Performance Report</h1>
+        <div class="container">
+            <div class="form-chart-container">
+                <div class="form-container">
+                    <label for="txtStudentID">Enter Student ID:</label>
+                    <input type="text" id="txtStudentID" runat="server" />
+                    <br />
+                    <label for="Text1">Enter Mark ID:</label>
+                    <input type="text" id="Text1" runat="server" />
+                    <br />
+                    <br />
+                    <asp:Button ID="btnGenerateReport" Text="Generate Mark Report" OnClick="btnGenerateReport_Click" runat="server" CssClass="button" />
+                </div>
+                <div class="chart-container">
+                    <asp:Chart ID="Chart1" runat="server" Height="190px" Width="273px">
+                        <Series>
+                            <asp:Series Name="Series1" ChartType="Pie" ChartArea="ChartArea1"></asp:Series>
+                        </Series>
+                        <ChartAreas>
+                            <asp:ChartArea Name="ChartArea1">
+                            </asp:ChartArea>
+                        </ChartAreas>
+                    </asp:Chart>
+                </div>
+            </div>
+        </div>
+        <br />
+        <asp:GridView ID="GridViewStudents" CssClass="gridview" runat="server" AutoGenerateColumns="False" OnSelectedIndexChanged="GridViewStudents_SelectedIndexChanged">
+            <Columns>
+                <asp:BoundField DataField="StudentID" HeaderText="Student ID" />
+                <asp:BoundField DataField="StudentName" HeaderText="Student Name" />
+                <asp:BoundField DataField="marks_id" HeaderText="Mark ID" />
+                <asp:BoundField DataField="class" HeaderText="Class" />
+                <asp:TemplateField HeaderText="Generate Report">
+                    <ItemTemplate>
+                        <asp:Button ID="btnGenerateStudentReport" Text="Generate Mark Report" CommandName="Select" CommandArgument='<%# Eval("StudentID").ToString() + ";" + Eval("marks_id").ToString() %>' runat="server" CssClass="button" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ student_performance.aspx.vb
@@ -0,0 +1,127 @@
+﻿Imports System.Data.SqlClient
+Imports System.Drawing
+Imports System.Web.UI.DataVisualization.Charting
+
+Public Class student_performance
+    Inherits System.Web.UI.Page
+    Dim conn As String = ConfigurationManager.ConnectionStrings("schoolmanagement").ConnectionString
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not IsPostBack Then
+            BindStudentData()
+        End If
+    End Sub
+    Protected Sub BindStudentData()
+        Using connection As New SqlConnection(conn)
+            Dim query As String = "SELECT StudentID,StudentName,marks_id,class
+                                    from StudentMarks"
+            Using command As New SqlCommand(query, connection)
+                connection.Open()
+                Dim adapter As New SqlDataAdapter(command)
+                Dim dataset As New DataSet()
+                adapter.Fill(dataset)
+                GridViewStudents.DataSource = dataset.Tables(0)
+                GridViewStudents.DataBind()
+            End Using
+        End Using
+    End Sub
+    Protected Sub btnGenerateReport_Click(sender As Object, e As EventArgs) Handles btnGenerateReport.Click
+        Dim selectedStudentID As Integer = Convert.ToInt32(txtStudentID.Value)
+        Dim selectedMarkID As Integer = Convert.ToInt32(Text1.Value)
+
+        Using connection As New SqlConnection(conn)
+            connection.Open()
+            Dim query As String = "SELECT tamil, science, maths, social, english FROM StudentMarks WHERE StudentID = @StudentID AND marks_id = @MarkID"
+
+            Using command As New SqlCommand(query, connection)
+                command.Parameters.AddWithValue("@StudentID", selectedStudentID)
+                command.Parameters.AddWithValue("@MarkID", selectedMarkID)
+
+                Using reader As SqlDataReader = command.ExecuteReader()
+                    Dim subjects As New List(Of String)()
+                    Dim marks As New List(Of Integer)()
+
+                    If reader.Read() Then
+                        Dim tamilMark As Integer = Convert.ToInt32(reader("tamil"))
+                        Dim scienceMark As Integer = Convert.ToInt32(reader("science"))
+                        Dim mathsMark As Integer = Convert.ToInt32(reader("maths"))
+                        Dim socialMark As Integer = Convert.ToInt32(reader("social"))
+                        Dim englishMark As Integer = Convert.ToInt32(reader("english"))
+
+                        subjects.Add("Tamil")
+                        subjects.Add("Science")
+                        subjects.Add("Maths")
+                        subjects.Add("Social")
+                        subjects.Add("English")
+
+                        marks.Add(tamilMark)
+                        marks.Add(scienceMark)
+                        marks.Add(mathsMark)
+                        marks.Add(socialMark)
+                        marks.Add(englishMark)
+
+                        Chart1.Series("Series1").Points.DataBindXY(subjects, marks)
+                        Chart1.Series("Series1").Label = "#VALY"
+                        Chart1.Series("Series1").ChartType = SeriesChartType.Pie
+                        Chart1.ChartAreas("ChartArea1").Area3DStyle.Enable3D = True
+                        Chart1.Legends.Add(New Legend("Legend1"))
+
+                        For i As Integer = 0 To subjects.Count - 1
+                            Chart1.Series("Series1").Points(i).LegendText = subjects(i)
+                        Next i
+                    End If
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Protected Sub GridViewStudents_SelectedIndexChanged(sender As Object, e As EventArgs) Handles GridViewStudents.SelectedIndexChanged
+        Dim selectedRow As GridViewRow = GridViewStudents.SelectedRow
+        Dim studentID As Integer = Convert.ToInt32(selectedRow.Cells(0).Text)
+        Dim markID As Integer = Convert.ToInt32(selectedRow.Cells(2).Text)
+        Using connection As New SqlConnection(conn)
+            connection.Open()
+            Dim query As String = "SELECT tamil, science, maths, social, english FROM StudentMarks WHERE StudentID = @StudentID AND marks_id = @MarkID"
+
+            Using command As New SqlCommand(query, connection)
+                command.Parameters.AddWithValue("@StudentID", studentID)
+                command.Parameters.AddWithValue("@MarkID", markID)
+
+                Using reader As SqlDataReader = command.ExecuteReader()
+                    Dim subjects As New List(Of String)()
+                    Dim marks As New List(Of Integer)()
+
+                    If reader.Read() Then
+                        Dim tamilMark As Integer = Convert.ToInt32(reader("tamil"))
+                        Dim scienceMark As Integer = Convert.ToInt32(reader("science"))
+                        Dim mathsMark As Integer = Convert.ToInt32(reader("maths"))
+                        Dim socialMark As Integer = Convert.ToInt32(reader("social"))
+                        Dim englishMark As Integer = Convert.ToInt32(reader("english"))
+
+                        subjects.Add("Tamil")
+                        subjects.Add("Science")
+                        subjects.Add("Maths")
+                        subjects.Add("Social")
+                        subjects.Add("English")
+
+                        marks.Add(tamilMark)
+                        marks.Add(scienceMark)
+                        marks.Add(mathsMark)
+                        marks.Add(socialMark)
+                        marks.Add(englishMark)
+
+                        Chart1.Series("Series1").Points.DataBindXY(subjects, marks)
+                        Chart1.Series("Series1").Label = "#VALY"
+                        Chart1.Series("Series1").ChartType = SeriesChartType.Pie
+                        Chart1.ChartAreas("ChartArea1").Area3DStyle.Enable3D = True
+                        Chart1.Legends.Add(New Legend("Legend1"))
+
+                        For i As Integer = 0 To subjects.Count - 1
+                            Chart1.Series("Series1").Points(i).LegendText = subjects(i)
+                        Next i
+                    End If
+                End Using
+            End Using
+        End Using
+    End Sub
+End Class
